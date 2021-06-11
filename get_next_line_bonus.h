c7de4cc8f5879767b2dd1c45a351833cb9ac36bd
Laura <lsteiner@student.42sp.org.br>
2021-06-11 12:12:01 -0300

    Mandatory part done

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,25 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lsteiner <lsteiner@students.42sp.org.br>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/07 20:33:10 by lsteiner          #+#    #+#             */
-/*   Updated: 2021/06/07 20:33:10 by lsteiner         ###   ########.fr       */
+/*   Created: 2021/06/11 12:10:23 by lsteiner          #+#    #+#             */
+/*   Updated: 2021/06/11 12:10:23 by lsteiner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# include <stdlib.h>
-# include <unistd.h>
 
-int		get_next_line(int fd, char **line);
-char	*ft_strdup(const char *src);
-size_t	ft_strlen(const char *str);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *s, int c);
-char	*ft_double_the_line(char *save, size_t size);
-
-#endif
---
+++ main.c
@@ -0,0 +1,50 @@
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <limits.h>
+
+int main(int argc, char **argv)
+{
+	int fd, ret, line_count;
+	char *line;
+
+	line_count = 1;
+	ret = 0;
+	line = NULL;
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		while ((ret = get_next_line(fd, &line)) > 0)
+		{
+			printf(" \n [ Return: %d ] | A line has been read #%d => %s\n", ret, line_count, line);
+			line_count++;
+			free(line);
+		}
+		printf(" \n [ Return: %d ] A line has been read #%d: %s\n", ret, line_count++, line);
+		printf("\n");
+		if (ret == -1)
+			printf("-----------\n An error happened\n");
+		else if (ret == 0)
+		{
+			printf("-----------\n EOF has been reached\n");
+			free(line);
+		}
+		close(fd);
+	}
+	if (argc == 1)
+	{
+		while ((ret = get_next_line(0, &line)) > 0)
+		{
+			printf("[Return: %d] Line #%d: %s\n", ret, line_count, line);
+			line_count++;
+		}
+		if (ret == -1)
+			printf("\n An error happened\n");
+		else if (ret == 0)
+			printf("\n EOF has been reached\n");
+		close(fd);
+	}
+	return (0);
+}
+
+
